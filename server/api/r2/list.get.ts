ec73d709c8ef268052769bc570480197dba1eb41
SuperKojima <hayato.kojima@steamship.co.jp>
2025-01-15 10:37:56 +0900

    エンドポイント追加・整理

---
+++ server/api/r2/list.get.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+    const r2 = event.context.cloudflare.env.R2;
+    const list = await r2.list();
+
+    return {
+        success: true,
+        files: list
+    };
+})
