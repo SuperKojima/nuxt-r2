94d0a053bb9aa1c6e65e7a4080c9c9aa4a5b715b
SuperKojima <hayato.kojima@steamship.co.jp>
2025-01-15 15:01:11 +0900

    大体満足

--- pages/upload.vue
+++ pages/upload.vue
@@ -1,28 +1,29 @@
 <script setup lang="ts">
 const upload = async (e: Event) => {
   const formData = new FormData(e.target as HTMLFormElement);
-  const response = await $fetch('/api/r2/v1', {
+  const response = await $fetch('/api/r2', {
     method: 'PUT',
     body: formData
   });
-  console.log(response);
+  navigateTo('/');
 };
 </script>
 
 <template>
   <div>
-    <h2 class="mb-4">/upload/v1</h2>
+    <h2 class="mb-4">/upload/v2</h2>
     <div>
-      <form @submit.prevent="upload" class="space-y-4">
-        <div>
-          <input 
-            type="file" 
-            name="image"
-            accept="image/*"
-            class="block w-full" 
-          />
-        </div>
-
+      <form
+        @submit.prevent="upload"
+        enctype="multipart/form-data"
+        class="bg-gray-100 p-4 rounded-md grid gap-4"
+      >
+        <input 
+          type="file"
+          multiple
+          name="images"
+          accept="image/*"
+        />
         <button 
           type="submit" 
           class="bg-blue-500 text-white p-2 rounded-md"
--- server/api/r2/index.get.ts
+++ server/api/r2/index.get.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
     const r2 = event.context.cloudflare.env.R2;
-    const list = await r2.list();
+    const list = await r2.list()
 
     return list
 })
